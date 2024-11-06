fdbe9a0e3207897fdf5a7396c10fd3ab2ec12fe8
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-11-06 10:38:18 +0530

    Updation Done

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,19 +5,19 @@ const AboutSection = () => {
   return (
     <div className="px-4 py-5 bg-gray-100">
       <div className="flex flex-col md:flex-row items-center md:pl-20 lg:pl-52">
-        <div className="flex-1 mb-8 md:mb-0">
-          <img src={about} alt="Cooking Tools" className="w-full md:w-96 object-cover mx-auto md:mx-0 hidden md:block"/>
+        <div className="flex-1 mb-8 md:mb-0 h-[400px] md:h-[450px]">
+          <img src={about} alt="Cooking Tools" className="w-full h-full md:w-[450px] object-cover mx-auto md:mx-0 hidden md:block"/>
         </div>
-        <div className="flex-1 text-center md:text-left gap-0">
-          <h2 className="text-2xl md:text-4xl font-bold mb-4" style={{color:'rgba(14,35,104,1)'}} >About Us</h2>
-          <p className="text-gray-600 mb-4 text-2xl md:text-2xl" style={{color:'rgba(68,73,87,1)'}} >
+        <div className="flex-1 text-center md:text-left md:mr-52">
+          <h2 className="text-2xl md:text-2xl font-bold mb-4" style={{color:'rgba(14,35,104,1)'}} >About Us</h2>
+          <p className="mb-4 text-xl md:text-xl" style={{color:'rgba(68,73,87,1)'}} >
             Lorem Ipsum is simply dummy text of the printing and <br/> 
             typesetting industry. Lorem Ipsum has been the industry's <br/>
             standard dummy text ever since the 1500s, when an unknown <br/>
             printer took a galley of type and scrambled it to make a type <br/> 
             specimen book. t has survived not only five centuries.
           </p>
-          <button className="bg-red-600 text-white font-bold rounded-full px-6 md:px-10 py-3 mt-5 mb-5 mr-">
+          <button className="bg-red-600 text-white font-bold rounded-full px-6 md:px-10 py-3 mt-5 mb-5">
             Read More
           </button>
         </div>
